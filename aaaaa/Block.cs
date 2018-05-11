2e975f2ea140c2b245793e482bf5f874e0c3f08b
ludidilu <ludidilu@163.com>
2018-05-11 15:20:21 +0800

    a

---
+++ aaaaa/Block.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Block : MonoBehaviour, IPointerClickHandler
+{
+    private int pos;
+
+    private bool isVertical;
+
+    private Action<int, bool> callBack;
+
+    public void Init(int _pos, bool _isVertical, Action<int, bool> _callBack)
+    {
+        pos = _pos;
+
+        isVertical = _isVertical;
+
+        callBack = _callBack;
+    }
+
+    public void OnPointerClick(PointerEventData _data)
+    {
+        callBack(pos, isVertical);
+    }
+}
---
+++ aaaaa/NewBehaviourScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Google.Protobuf;
+using System;
+using LitJson;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private const int totalMapWidth = 500;
+
+    public RectTransform container;
+
+    public InputField widthInput;
+
+    public InputField heightInput;
+
+    public GameObject confirmWidthAndHeightPanel;
+
+    private int mapWidth;
+
+    private int mapHeight;
+
+    void Start()
+    {
+
+
+    }
+
+    public void ConfirmWidthAndHeight()
+    {
+        confirmWidthAndHeightPanel.SetActive(false);
+
+        mapWidth = int.Parse(widthInput.text);
+
+        mapHeight = int.Parse(heightInput.text);
+
+        Create();
+    }
+
+    private void Create()
+    {
+        float unitWidth = totalMapWidth / mapWidth;
+
+        for (int i = 0; i < mapHeight; i++)
+        {
+            for (int m = 0; m < mapWidth; m++)
+            {
+                GameObject go = new GameObject();
+
+                Image img = go.AddComponent<Image>();
+
+                go.transform.SetParent(container, false);
+
+                img.rectTransform.sizeDelta = new Vector2(unitWidth, unitWidth);
+
+                img.rectTransform.pivot = new Vector2(0, 1);
+
+                img.rectTransform.anchoredPosition = new Vector2(m * unitWidth, i * -unitWidth);
+
+                if (i % 2 == m % 2)
+                {
+                    img.color = Color.gray;
+                }
+            }
+        }
+
+        container.anchoredPosition = new Vector2(-0.5f * totalMapWidth, 0.5f * totalMapWidth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
